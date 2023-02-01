250af9982afc1b350ba11a08868aff6bca537dd5
R-Rangel <r.rangel.0222@gmail.com>
2023-02-01 17:03:34 -0600

    new

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var startButton = document.querySelector('.button')
+
+startButton.addEventListener('click', startQuiz)
+
+
+function startQuiz(){
+console.log('started')
+startButton.classList.add('remove')
+}
+
+function nextQuestion(){
+
+
+
+}
+
+function answerSelect(){
+
+
+
+}
